2829a2d10d73827619481603c1abf367b208591a
Tevin <skeetet1@hawkmail.newpaltz.edu>
2022-01-27 17:50:12 -0500

    ADD: Sections for the different TESLAS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from "./components/Header"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
-     <h1>WHatsup</h1>
+   
+      {/* <Header /> */}
+      
+      <Home />
+
     </div>
   );
 }
